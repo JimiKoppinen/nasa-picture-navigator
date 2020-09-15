957cc206538e521b3966d07e77be3c35b01e1aa9
JimiKoppinen <60182244+JimiKoppinen@users.noreply.github.com>
2020-09-15 21:52:24 +0300

    Epic-komponentti

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import HakuKentta from "./HakuKentta";
 import POD from "./POD";
 import Navi from "./Navi";
 import Home from "./Home";
+import Epic from "./Epic";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
@@ -18,8 +19,11 @@ class App extends React.Component {
             <Route path="/POD">
               <POD />
             </Route>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/EPIC">
+              <Epic />
             </Route>
           </Switch>
         </Router>
